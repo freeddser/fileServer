ae5b99ab29441fba09b9c7c4828b96ed395b62cb
gavin <gavin@example.com>
2019-12-11 15:36:37 +0800

    add static path

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+####
+    go get github.com/freeddser/fileServer/client
+    go get github.com/freeddser/fileServer/server
+    
 #### Server
-    go run Server.go --port 5050
+    ./server.go --port 5050
   
 #### Client
-    go run Client.go --server http://127.0.0.1:5050 --filename a.txt
+    ./client.go --server http://127.0.0.1:5050 --filename a.txt
--- server.go
+++ server.go
@@ -10,13 +10,13 @@ import (
 	"time"
 )
 
-const static_path = "server/upload"
+const static_path = "upload"
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	t := time.Now().Unix()
+	t := time.Unix(time.Now().Unix(), 0).Format("2006-01-02-15-04-05")
 	name := r.URL.Query().Get("name")
 	if name != "" {
-		fileName := strconv.FormatInt(t, 10) + "_" + name
+		fileName := t + "_" + name
 		file, err := os.Create(static_path + "/" + fileName)
 		if err != nil {
 			panic(err)
@@ -25,6 +25,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
+		log.Println(fileName + " upload success")
 		w.Write([]byte(fileName + " upload success"))
 	} else {
 		w.Write([]byte("Please input Filename"))
@@ -34,9 +35,12 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/upload", uploadHandler)
+	http.Handle("/", http.FileServer(http.Dir("upload")))
 	var port int
 	flag.IntVar(&port, "port", 5050, "server port, default 5050")
 	flag.Parse()
 	log.Print("Server running on port:", port)
+
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+
 }
