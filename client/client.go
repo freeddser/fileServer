12925e1f7eedc4215da87d187964f32000cda9b7
gavin <gavin@example.com>
2019-12-27 15:00:23 +0800

    create binary file for ubuntu

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,6 +15,11 @@ func main() {
 	flag.StringVar(&server, "server", "http://127.0.0.1:5050", "server address, default:http://127.0.0.1:5050")
 	flag.StringVar(&fileName, "filename", "", "filename and path: /tmp/a.txt")
 	flag.Parse()
+	if fileName==""{
+		fmt.Println("\n\nUse -h to get more information on command line options\n")
+		fmt.Println("You must specify a filename")
+		os.Exit(1)
+	}
 	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
